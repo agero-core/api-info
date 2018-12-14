183989c20af05fe29f4341cbc2cbd16d4fb58731
Vyshak Srishylappa <vyshaksrishyla@gmail.com>
2018-12-14 13:30:34 -0500

    Code: Added GettingStartedTests.cs

---
+++ Agero.Core.ApiInfo.Tests/Helpers/ConstantHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Agero.Core.ApiInfo.Tests.Helpers
+{
+    public static class ConstantHelper
+    {
+        public const string ApplicationName = "TestApplicaton";
+        public const string ApplicationVersion = "1.0.0.0";
+    }
+}
