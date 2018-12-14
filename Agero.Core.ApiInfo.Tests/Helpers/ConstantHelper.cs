95a8810bd971ecab0a637a2f047aed103ebde415
Vyshak Srishylappa <vyshaksrishyla@gmail.com>
2018-12-14 14:47:56 -0500

    Tests: Updated Test Cases.

--- Agero.Core.ApiInfo.Tests/Helpers/ConstantHelper.cs
+++ Agero.Core.ApiInfo.Tests/Helpers/ConstantHelper.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Agero.Core.ApiInfo.Tests.Helpers
+﻿namespace Agero.Core.ApiInfo.Tests.Helpers
 {
     public static class ConstantHelper
     {
         public const string ApplicationName = "TestApplicaton";
         public const string ApplicationVersion = "1.0.0.0";
-        public static readonly object CustomerApplicaitonInformation = new {
+        public static readonly object CustomerApplicationInformation = new {
             customApplicationDescription = "Application description specific to the application",
             customerApplicationInfo = "Application Info specific to the application"
         };
---
+++ Agero.Core.ApiInfo.Tests/Tests/ApiInfoComposerTests.cs
@@ -0,0 +1,43 @@
+﻿using Agero.Core.ApiInfo.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agero.Core.ApiInfo.Tests
+{
+    [TestClass]
+    public class ApiInfoComposerTests
+    {
+        [TestMethod]
+        public void Get_When_ApiInformation_With_ApplicationInformation_Null()
+        {
+            //Arrange
+            IApiInfoComposer composer = new ApiInfoComposer(ConstantHelper.ApplicationName,
+                ConstantHelper.ApplicationVersion, () => null);
+
+            //Act
+            ApiInformation apiInformation = composer.Get();
+
+            //Assert
+            Assert.IsNotNull(apiInformation);
+            Assert.AreEqual(ConstantHelper.ApplicationName, apiInformation.Name);
+            Assert.AreEqual(ConstantHelper.ApplicationVersion, apiInformation.Version);
+            Assert.IsNull(apiInformation.Application);
+        }
+
+        [TestMethod]
+        public void Get_When_ApiInformation_With_ApplicationInformation_Not_Null()
+        {
+            //Arrange
+            IApiInfoComposer composer = new ApiInfoComposer(ConstantHelper.ApplicationName,
+                ConstantHelper.ApplicationVersion, () => ConstantHelper.CustomerApplicationInformation);
+
+            //Act
+            ApiInformation apiInformation = composer.Get();
+
+            //Assert
+            Assert.IsNotNull(apiInformation);
+            Assert.AreEqual(ConstantHelper.ApplicationName, apiInformation.Name);
+            Assert.AreEqual(ConstantHelper.ApplicationVersion, apiInformation.Version);
+            Assert.IsNotNull(apiInformation.Application);
+        }
+    }
+}
